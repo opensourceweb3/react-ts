a222b0783a22e209f50705fab5b237d8dbb2a25e
wangl_mvalley <wangl@mvalley.io>
2020-05-21 10:12:05 +0800

    init the projects

---
+++ .history/src/components/foo/foo_20200521092640.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Foo: React.FC = () => {
+  return <div>this is the foo component.</div>;
+};
+
+export default Foo;
---
+++ .history/src/components/foo/foo_20200521092821.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./style.css";
+
+const Foo: React.FC = () => {
+  return <div className={styles.fooRoot}>this is the foo component.</div>;
+};
+
+export default Foo;
